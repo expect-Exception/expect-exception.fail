8ace74d42ff89a50b9232193af4faff8ebe5f989
Vannsl <boehner.vanessa@gmail.com>
2020-04-04 18:39:48 +0200

    chore: chang babel presets

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,12 @@ module.exports = {
       "@babel/preset-env",
       {
         targets: {
-          node: "current"
-        }
+          edge: "17",
+          firefox: "60",
+          chrome: "67",
+          safari: "11.1",
+        },
+        useBuiltIns: "usage",
       }
     ]
   ]
