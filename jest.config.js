c2e3e819388bc1575a2ad74c8f5b9f45ccf88a6e
Vannsl <boehner.vanessa@gmail.com>
2020-06-30 18:19:14 +0200

    chore: prettier linting

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   transform: {
     "^.+\\.svelte$": "jest-transform-svelte",
-    "^.+\\.js$": "babel-jest"
+    "^.+\\.js$": "babel-jest",
   },
   moduleFileExtensions: ["js", "svelte"],
   testPathIgnorePatterns: ["node_modules", "cypress"],
   bail: false,
   verbose: true,
   transformIgnorePatterns: ["node_modules"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 };
