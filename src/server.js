7af4950fb251e3187a9bada5f395b96a170c8e82
Vannsl <boehner.vanessa@gmail.com>
2020-11-12 17:40:15 +0100

    chore: redirect feed inside of sapper

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import sirv from "sirv";
 import polka from "polka";
+import redirect from("@polka/redirect");
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
+polka()
+  .get("/feed.xml", (req, res) => {
+    redirect(res, "https://g8qwn1.podcaster.de/expect-exception.rss");
+  })
+  .get("static/feed.xml", (req, res) => {
+    redirect(res, "https://g8qwn1.podcaster.de/expect-exception.rss");
+  })
   .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
