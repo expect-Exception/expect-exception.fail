18120368cdc74e243c2dd46c9f3ad4b7e5569c3e
Vannsl <boehner.vanessa@gmail.com>
2020-11-13 07:57:04 +0100

    chore: add status code 301 for redirects

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ const dev = NODE_ENV === "development";
 
 polka()
   .get("/feed.xml", (req, res) => {
-    redirect(res, "https://g8qwn1.podcaster.de/expect-exception.rss");
+    redirect(res, 301, "https://g8qwn1.podcaster.de/expect-exception.rss");
   })
   .get("static/feed.xml", (req, res) => {
-    redirect(res, "https://g8qwn1.podcaster.de/expect-exception.rss");
+    redirect(res, 301, "https://g8qwn1.podcaster.de/expect-exception.rss");
   })
   .use(
     compression({ threshold: 0 }),
